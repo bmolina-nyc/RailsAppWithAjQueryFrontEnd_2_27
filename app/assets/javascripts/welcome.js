75ef745057504cfc9433e9de366edfada5b8138d
bmolina-nyc <bruce.molina.81@gmail.com>
2017-03-01 23:18:19 -0500

    messing around with jquery form replacement

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,19 +1,36 @@
 $(function() {
-  toggleForm();
+  toggleLogin();
+  toggleSignup();
 })
 
 
-function toggleForm(){
-$('a').on("click", function(){
-  event.preventDefault();
-  console.log("test")
-    if ($('div#signup')[0].style.cssText === "display: block;") {
-      $('div#signup')[0].style = "display: none;"
-      $('div#login')[0].style= "display: block;"
-    } else if ($('div#signup')[0].style.cssText === "display: none;"){
-      $('div#signup')[0].style = "display: block;";
-      $('div#login')[0].style = "display: none;";
-    }
-  })
+function toggleLogin(){
+
+  $('a').on("click", function(event){
+    event.preventDefault();
+
+      if ($('div#signup')[0].style.cssText === "display: block;" || $('div#signup')[0].style.cssText === "display:block;") {
+        
+        $('div#signup')[0].style = "display: none;"
+        $('div#login')[0].style = "display: block;"
+        $('h3').replaceWith('<h3>Login or <a onClick="toggleSignup" href="#">Sign Up</a></h3>');
+      } 
+    });
+
+}
+
+
+function toggleSignup(){
+
+  $('a').on("click", function(event){
+    event.preventDefault();
+
+   if ($('div#login')[0].style.cssText === "display: none;" || $('div#login')[0].style.cssText === "display:none;") {
+       
+        $('div#signup')[0].style = "display: block;";
+        $('div#login')[0].style = "display: none;";
+        $('h3').replaceWith('<h3>Sign Up or  <a onClick="toggleLogin" href="#">Login</a> </h3>');
+      }
+    });
 
 }
