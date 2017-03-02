1e09e9d713dfd2aeb9a576394f83651a1886b1c8
bmolina-nyc <bruce.molina.81@gmail.com>
2017-03-01 20:44:39 -0500

    first ajax complete - event prevent default

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,19 @@
 $(function() {
-  $('a').on("click", function(){
-    toggleForm()
-  })
+  toggleForm();
 })
 
 
 function toggleForm(){
-  console.log("works")
+$('a').on("click", function(){
+  event.preventDefault();
+  console.log("test")
+    if ($('div#signup')[0].style.cssText === "display: block;") {
+      $('div#signup')[0].style = "display: none;"
+      $('div#login')[0].style= "display: block;"
+    } else if ($('div#signup')[0].style.cssText === "display: none;"){
+      $('div#signup')[0].style = "display: block;";
+      $('div#login')[0].style = "display: none;";
+    }
+  })
+
 }
