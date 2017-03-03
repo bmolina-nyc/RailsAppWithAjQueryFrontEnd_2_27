b3ad450159db1ddb7c8b28533a92d873a5686aa2
bmolina-nyc <bruce.molina.81@gmail.com>
2017-03-02 19:07:03 -0500

    working on a trips index page - added images and basic html structure

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,19 +1,18 @@
 $(function() {
   toggleLogin();
-  toggleSignup();
 })
 
 
 function toggleLogin(){
 
   $('a').on("click", function(event){
-    event.preventDefault();
+    event.preventDefault(event);
+        $('h3').replaceWith('<h3>Login or <a onClick="toggleSignup" href="#">Sign Up</a></h3>');
 
       if ($('div#signup')[0].style.cssText === "display: block;" || $('div#signup')[0].style.cssText === "display:block;") {
         
         $('div#signup')[0].style = "display: none;"
         $('div#login')[0].style = "display: block;"
-        $('h3').replaceWith('<h3>Login or <a onClick="toggleSignup" href="#">Sign Up</a></h3>');
       } 
     });
 
@@ -23,13 +22,13 @@ function toggleLogin(){
 function toggleSignup(){
 
   $('a').on("click", function(event){
-    event.preventDefault();
+        $('h3').replaceWith('<h3>Sign Up or  <a onClick="toggleLogin" href="#">Login</a> </h3>');
+    event.preventDefault(event);
 
    if ($('div#login')[0].style.cssText === "display: none;" || $('div#login')[0].style.cssText === "display:none;") {
        
         $('div#signup')[0].style = "display: block;";
         $('div#login')[0].style = "display: none;";
-        $('h3').replaceWith('<h3>Sign Up or  <a onClick="toggleLogin" href="#">Login</a> </h3>');
       }
     });
 
