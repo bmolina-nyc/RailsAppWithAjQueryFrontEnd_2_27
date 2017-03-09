17686a781b7a3553113d20684906c2f20d546f61
bmolina-nyc <bruce.molina.81@gmail.com>
2017-03-08 21:32:01 -0500

    cleaned up code btwn index to show page - cash is green

--- app/assets/javascripts/trips_index.js
+++ app/assets/javascripts/trips_index.js
@@ -4,12 +4,14 @@
      var id = $(this).data("id")
 
      $.get("/trips/" + id + ".json", function(data) {
-      $(`li#${id} h6`).html(data.description + " test")
+      $(`li#${id} h6`).html(data.description + " -  " + `<strong id="cash">$${randomPrice()}</strong>`)
       })/* ajax request end */
     }) /* button on click end */
   }) /* top level function end */
 
 
 
-
-// REPLACE TEST ABOVE WITH A NEW FUNCTION THAT APPENDS A RANDOM PRICE 
+/* get a cash value for a trip */
+  function randomPrice(){
+    return Math.floor(Math.random() * 1000)
+  }
