641b4e699624d01fa06d7c699fdc362e6243cc94
bmolina-nyc <bruce.molina.81@gmail.com>
2017-03-11 15:28:33 -0500

    added the has_many json logic to the users show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,28 @@ class PostsController < ApplicationController
   # route is currently set to only show your comments on your personal page
   def index
     @posts = current_user.posts
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @posts.to_json(only)}
+    end
+  end
+
+    def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post }
+                  
+    end
   end
 
   def create 
     @post = Post.create(post_params)
-    @post.user_id = current_user.user_id
+    @post.user_id = current_user.id
     @post.save
+
+    redirect_to user_path(current_user)
   end
 
 
